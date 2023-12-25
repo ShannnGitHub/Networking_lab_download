71778db891f7734d2979db1c7abd93e7aaa4fe5e
ShannnGitHub <143635518+ShannnGitHub@users.noreply.github.com>
2023-12-25 23:15:41 +0530

    file_server.py

--- file_server.py
+++ file_server.py
@@ -8,8 +8,12 @@ directory = '.'
 port = 8000
 
 Handler = http.server.SimpleHTTPRequestHandler
+Handler.extensions_map.update({
+    '.html': 'text/html',
+})
 
+# Change the working directory to your specified directory
 with socketserver.TCPServer(("", port), Handler) as httpd:
     print(f"Serving on port {port}")
-    print(f"Open your browser and go to http://localhost:{port}/index.html")
+    print(f"Open your browser and go to http://localhost:{port}/")
     httpd.serve_forever()
